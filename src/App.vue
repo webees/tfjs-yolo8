e15609e49c351a20a04e026ceb5b275a008df1e5
webees <5155291+webees@users.noreply.github.com>
2023-09-16 03:53:16 +0800

    showLoadingToast

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,37 @@
 <template>
+  <van-toast></van-toast>
   <router-view />
   <tab-bar v-show="$route.meta.tabbar" />
 </template>
 
 <script lang="ts" setup>
-import { watch } from 'vue'
-import { useRoute } from 'vue-router'
+import { showLoadingToast, closeToast } from 'vant'
 import i18n from '@/vue-i18n'
+import { app } from '@/vue-pinia'
 
-const route = useRoute()
-
+// title
 watch(
-  () => route.meta.title,
+  () => useRoute().meta.title,
   v => {
     if (v) {
       window.document.title = i18n.t(v as string)
+    }
+  },
+  { immediate: true }
+)
+
+// loading
+watch(
+  () => ref(app().loading),
+  v => {
+    if (v) {
+      showLoadingToast({
+        message: '加载中...',
+        forbidClick: true,
+        duration: 0
+      })
+    } else {
+      closeToast()
     }
   },
   { immediate: true }
