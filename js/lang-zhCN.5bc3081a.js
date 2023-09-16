00957f9bc432c920ce08cf7f2c7a6e9d3acf0baa
webees <5155291+webees@users.noreply.github.com>
2023-09-16 18:30:13 +0000

    Deploying to cf-pages from @ webees/tfjs-yolo8@bfc4b38a9de06e8bf4ef12d9b88e21f377e10d00 🚀

---
+++ js/lang-zhCN.5bc3081a.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackChunktfjs_yolov8"]=self["webpackChunktfjs_yolov8"]||[]).push([[917],{8520:function(e,a,o){o.r(a),a["default"]={zhCN:{_tabbar:{image:"图片",video:"视频",webcam:"摄像"},image:"图片",video:"视频",webcam:"摄像","Open Image":"打开图片","Open Video":"打开视频"}}}}]);
